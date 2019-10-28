fb580d43a906e4d21f336d31a6346baa74647151
Johan Guldmyr <martbhell@users.noreply.github.com>
2019-10-28 14:47:52 +0200

    Revert "Set python2-barbicanclient for RedHat "

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,7 +9,7 @@ class barbican::params {
       $api_service_name             = 'openstack-barbican-api'
       $worker_package_name          = 'openstack-barbican-worker'
       $worker_service_name          = 'openstack-barbican-worker'
-      $client_package_name          = 'python2-barbicanclient'
+      $client_package_name          = 'python-barbicanclient'
       $barbican_wsgi_script_path    = '/var/www/cgi-bin/barbican'
       $barbican_wsgi_script_source  = '/usr/lib/python2.7/site-packages/barbican/api/app.wsgi'
       $dogtag_client_package        = 'pki-base'
